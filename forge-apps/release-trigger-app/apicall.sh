400a5116111ee8abca8b3e69358c4e576188c1df
Salas123 <salasii.jesus@gmail.com>
2023-11-27 13:56:50 -0800

    removed bash files

--- forge-apps/release-trigger-app/apicall.sh
+++
@@ -1,5 +0,0 @@
-curl https://api.openai.com/v1/threads/thread_pZEo1StRcjWtInwHoJTA6kTO/messages \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 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" \
-  -H "OpenAI-Beta: assistants=v1"
-
--- forge-apps/release-trigger-app/apicall2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --request GET --url 'https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=YreEtD8NInHf7dq8tFgnJ7ECKmpuY01q&scope=read%3Ame&redirect_uri=https%3A%2F%2Fjsii-cb.com&state=ai_automated3939&response_type=code&prompt=consent'
