ec79cb62e40fc8cf29edc09517087404c7f58ab8
Salas123 <salasii.jesus@gmail.com>
2023-11-27 13:40:59 -0800

    Integrating AI summarization functionality into release-trigger-app

--- forge-apps/release-trigger-app/src/index.jsx
+++ forge-apps/release-trigger-app/src/index.jsx
@@ -1,71 +1,193 @@
 import ForgeUI, {render, Fragment, Text, IssuePanel, Button, useProductContext, useState} from '@forge/ui';
 import api, {route} from "@forge/api";
+import CreateConfluenceDoc from "./createConfluenceDoc";
+import validateTicketStatus from "./utils/ValidateTicketStatus";
+import GetIssueData from "./utils/GetIssueData";
+import DBConn from "./utils/DBConn";
+import GetGroupData from "./utils/GetGroupData";
+import AIHelper from "./ai/AIHelper";
+import {FindInDB} from "./utils/DB";
+import {PackageContent} from "./utils/PackageContent";
 
-// how to implement...
-// 1. check context
-// 2. check release
-// 3. if not of the above, then
-// display button (if button pressed then trigger flag)
+async function triggerRelease({...props}){
+    // TODO: Add timeout func after
+    props.setReleaseStatus('...gathering story tickets...');
 
-async function getAPI(...props){
-    const project = 'JSII'
-    console.log(`Issue ID: ${props[0].issueID}`)
-    const JQLResponse =  await api.asUser().requestJira(route`/rest/api/2/search?jql=project%20%3D%20${project}%20and%20parent=${props[0].issueID}`, {
+    const JQLResponse =  await api.asUser().requestJira(route`/rest/api/2/search?jql=project%20%3D%20${process.env.JIRAPROJCETKEY}%20and%20parent=${props.issueState.issueID}`, {
         headers:{
             'Accept':'application/json'
         }
     });
 
     const JQLPayload =  await JQLResponse.json();
-    const childIssues = JQLPayload['issues'];
+    const issues = JQLPayload['issues'];
+
+    // filter out required fields
+    const childIssues = issues.map((entry) =>{
+        const obj = {};
+        obj.key = entry.key;
+        obj.id = entry.id;
+        obj.fields = entry.fields
+
+        return obj;
+    });
 
     if(childIssues.length === 0){
         console.log('No child issues found');
         // TODO: add error handling
     }
 
-    const issueResponse = await api.asUser().requestJira(route`/rest/api/2/issue/${props[0].issueID}`, {
-        headers:{
-            'Accept':'application/json'
-        }
+    // TODO: Add timeout func after
+    props.setReleaseStatus('...validating statuses are marked complete...');
+
+    // validate ticket statuses
+    if(!validateTicketStatus({tickets: childIssues, statusToBe: 'Done'})){
+        //TODO: add error handling for invalid status for child tickets
+        console.log('Invalid status... all ticket statuses must be set to Done')
+    }
+
+    // Query members that will be assigned as reviewers
+    const memberData = await GetGroupData({groupName: 'release-reviewers'});
+
+    // Query current epic context
+    const epicData = await GetIssueData({issueID: props.issueState.issueID});
+
+    // TODO: Add timeout func after
+    props.setReleaseStatus('...creating Confluence doc...');
+
+    const ai = new AIHelper({
+        apiKey: process.env.AIAPIKEY,
+        setReleaseStatus: props.setReleaseStatus
     });
 
-    const issuePayload = await issueResponse.json();
-    console.log(issuePayload['fields']['fixVersions']);
+    const summary = await ai.createSummary(PackageContent({
+        epicDescription: epicData.fields.description.content[0].content[0].text,
+        userStoryDescriptions: childIssues.map((issue) => {
+            return issue.fields.description;
+        })
+    }));
 
-}
-function checkValidContext(...props) {
 
-    if (props[0].releaseFlag || props[0].issueType !== "Epic"){
-        // TODO: query fix versions and see if they have already been released
-        return false;
+    //instance to create a confluence doc
+    const createConfluenceDocInstance = new CreateConfluenceDoc({
+        docTitle: props.fixVersions[0],
+        summary: summary,
+        childIssues: childIssues,
+        members: memberData
+    });
+
+    // TODO: Add timeout func after
+    props.setReleaseStatus('...gathering release details to post to DB ...')
+    const confluencePageDetails = await createConfluenceDocInstance.createDoc();
+
+
+    const epicDetails = {
+        epicID: epicData.id,
+        epicKey: epicData.key,
+        childIssues: childIssues
     }
-    else
-        return true;
+
+    const DBInst = new DBConn({
+      releaseName: props.fixVersions[0],
+      confluenceDetails: confluencePageDetails,
+      epicDetails: epicDetails
+    });
+
+    // TODO: Add timeout func after
+    props.setReleaseStatus('...posting details to DB...')
+    await DBInst.getDB();
+
+    props.setReleaseState(true);
+    props.setContextState(false);
+
+    // TODO: Add timeout func after
+    props.setReleaseStatus('...done...')
 
 }
+
+async function checkReleaseState({...props}){
+    console.log('Checking if version has already been released...');
+    const fixVersions = props.fixVersions;
+
+    const res = await FindInDB({query: fixVersions[0]});
+
+    const hasBeenReleased = (res.document !== null);
+    console.log(`${fixVersions[0]} has been released: ${hasBeenReleased}`)
+    return hasBeenReleased;
+}
+
+async function getFixVersions({...props}){
+    const response = await api.asUser().requestJira(route`/rest/api/3/issue/${props.issueState.issueID}`);
+    const payload = await response.json();
+    const fixVersionsObjs = payload['fields']['fixVersions'];
+    let fixVersions = [];
+    for(let i = 0; i < fixVersionsObjs.length; i++){
+        fixVersions.push(fixVersionsObjs[i]['name']);
+    }
+
+    return fixVersions;
+}
+
+async function checkValidContext({...props}) {
+
+    return !(props.releaseState || props.issueState.issueType !== "Epic");
+
+}
+
+function getFixVersionsString(fixVersionsArr){
+    let fixVersionsStr = '';
+    for (let i = 0; i < fixVersionsArr.length; i++) {
+        fixVersionsStr += fixVersionsArr[i];
+    }
+
+    return fixVersionsStr;
+}
+
 
 const App = () => {
 
 
     const context = useProductContext();
-    const [appState, setAppState] = useState({
-        releaseFlag: false,
+
+    const [issueState, setissueState] = useState({
         issueType: context.platformContext.issueType,
         issueID: context.platformContext.issueId,
     });
-    const [contextState] = useState(checkValidContext(appState));
+
+    const [fixVersions] = useState(async () => await getFixVersions({issueState: issueState}));
+
+    const [releaseState, setReleaseState] = useState(async () => await checkReleaseState({fixVersions: fixVersions}));
+
+    const [contextState, setContextState] = useState(async () => await checkValidContext({
+        issueState: issueState,
+        releaseState: releaseState
+    }));
+
+    const [releaseStatus, setReleaseStatus] = useState(null);
+
 
   return (
     <Fragment>
         {contextState ?
             (<Fragment>
-                <Text>This epic is ready for release, press below to start release process </Text>
-                <Button text={'Start'} onClick={async () => await getAPI(appState)}/>
+                <Text>This epic is ready for release, press below to start release process for the following version(s): {getFixVersionsString((fixVersions))}</Text>
+                <Button text={'Start Release Process'}
+                        onClick={async () => await triggerRelease({issueState: issueState,
+                            fixVersions: fixVersions,
+                            setReleaseState: setReleaseState,
+                            setContextState: setContextState,
+                            setReleaseStatus: setReleaseStatus
+                        })}/>
             </Fragment>):
             (
                 <Fragment>
-                    <Text>This is not a valid issue or this epic has already been released</Text>
+                    <Text>Release has been initiated...{releaseStatus}</Text>
+                    {
+                        /*
+                        * TODO: Add handling on checking either if this an incorrect ticket type or this feature has
+                        *  already been released
+                        * */
+                    }
                 </Fragment>
             )
         }
